6b9d5b759ee46eff9553042ed3ff8a89c4c1f7ef
name <email>
2023-01-17 13:46:19 -0800

    Implement spillover, if lines exceed lines in pattern then go to next pattern

--- src/trackerlang/test/trackerlang.lua
+++ src/trackerlang/test/trackerlang.lua
@@ -3,6 +3,14 @@ local std = require 'std'
 
 local compiler = trackerlang.compiler()
 
+function os.capture(cmd)
+    -- stolen from https://gist.github.com/dukeofgaming/453cf950abd99c3dc8fc
+    local handle = assert(io.popen(cmd, 'r'))
+    local output = assert(handle:read('*a'))
+    handle:close()
+    return output
+end
+
 std.foreach(
     function(_, name) 
         local test = {
@@ -11,7 +19,7 @@ std.foreach(
         test.input = std.json.decode(std.file.tostring("test/" .. name .. "/input.json"))
         test.actual = std.executor(std.array('trackerlang.compiler'))(test.input)
         local fd = io.open("test/"..name.."/"..name..".log", "w")
-        fd:write(std.json.encode(test.actual))
+        fd:write(os.capture("echo '"..std.json.encode(test.actual).."' | jq"))
         fd:close()
         test.expected = std.json.decode(std.file.tostring("test/" .. name .. "/output.json"))
         test.error = not std.equals(test.expected, test.actual)
