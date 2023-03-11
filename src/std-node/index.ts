c68656a17d7b988f3f879aff1225c4f1445a0d33
name <email>
2023-03-10 20:38:09 -0800

    2023-03-10T20:38:09-08:00

--- src/std-node/index.ts
+++
@@ -1,6 +0,0 @@
-import { Server } from "./Server";
-import { File } from "./File";
-import { Client } from "./Client";
-
-export * from "../../common/std";
-export { Client, Server, File };
--- src/std-web/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "../../common/std";
-
-export * from "./Html";
